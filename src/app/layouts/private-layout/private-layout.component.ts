4362cb51450e20f9b43eb43f4f6c598497a3a696
Brenno Ramos <brennoramos231@gmail.com>
2026-06-12 17:21:12 -0300

    Att

--- src/app/layouts/private-layout/private-layout.component.ts
+++ src/app/layouts/private-layout/private-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from '../../componentes/sidebar/sidebar.component';
+import {SidebarItem} from '../../componentes/sidebar-item/sidebar-item.component';
 
 @Component({
   selector: 'app-private-layout',
@@ -10,5 +11,13 @@ import {SidebarComponent} from '../../componentes/sidebar/sidebar.component';
   styleUrl: './private-layout.component.scss'
 })
 export class PrivateLayoutComponent {
+
+  items: SidebarItem[] = [
+    {label: 'Menu', icon: 'home'},
+    {label: 'Bandas', icon: 'people'},
+    {label: 'Álbuns', icon: 'album'},
+    {label: 'Músicas', icon: 'music_note'},
+  ];
+
 
 }
