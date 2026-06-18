70acbddfada2ceffa3f6452e2f9f3d4e3227eb61
Brenno Ramos <brennoramos231@gmail.com>
2026-06-18 17:52:54 -0300

    Ajuste privateLayout

--- src/app/views/menu-view/menu-view.component.ts
+++ src/app/views/menu-view/menu-view.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SidebarComponent} from '../../componentes/sidebar/sidebar.component';
 import {BotaoComponent} from '../../componentes/botao/botao.component';
+import {CardComponent} from '../../componentes/card.component/card.component.component';
 
 @Component({
   selector: 'app-menu-view',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, BotaoComponent],
+  imports: [CommonModule, SidebarComponent, BotaoComponent, CardComponent],
   templateUrl: './menu-view.component.html',
   styleUrl: './menu-view.component.scss'
 })
