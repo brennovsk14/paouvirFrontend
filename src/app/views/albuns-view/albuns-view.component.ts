8a34502227a43ec0d3c2d397e63ac38a9f4af350
Brenno Ramos <brennoramos231@gmail.com>
2026-06-23 17:49:17 -0300

    View bandas e View Albuns

--- src/app/views/albuns-view/albuns-view.component.ts
+++ src/app/views/albuns-view/albuns-view.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {CardComponent} from '../../componentes/card-component/card-component';
+import {BotaoComponent} from '../../componentes/botao/botao.component';
 
 @Component({
   selector: 'app-albuns-view',
-  imports: [],
+  imports: [
+    CardComponent,
+    BotaoComponent
+  ],
   templateUrl: './albuns-view.component.html',
   styleUrl: './albuns-view.component.scss'
 })
--- src/app/views/bandas-view/bandas-view.component.ts
+++ src/app/views/bandas-view/bandas-view.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BotaoComponent} from '../../componentes/botao/botao.component';
+import {CardComponent} from '../../componentes/card-component/card-component';
 
 @Component({
   selector: 'app-bandas-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BotaoComponent, CardComponent],
   templateUrl: './bandas-view.component.html',
   styleUrl: './bandas-view.component.scss'
 })
