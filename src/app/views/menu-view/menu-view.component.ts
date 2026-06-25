9853332ef1097d4115f5e7456fcbe92e83d83a59
Brenno Ramos <brennoramos231@gmail.com>
2026-06-25 14:59:50 -0300

    Services

--- src/app/views/menu-view/menu-view.component.ts
+++ src/app/views/menu-view/menu-view.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SidebarComponent} from '../../componentes/sidebar/sidebar.component';
 import {BotaoComponent} from '../../componentes/botao/botao.component';
 import {CardComponent} from '../../componentes/card-component/card-component';
+import {BandaServiceService} from '../../services/banda-service.service';
+import {AlbumServiceService} from '../../services/album-service.service';
 
 @Component({
   selector: 'app-menu-view',
@@ -11,7 +13,34 @@ import {CardComponent} from '../../componentes/card-component/card-component';
   templateUrl: './menu-view.component.html',
   styleUrl: './menu-view.component.scss'
 })
-export class MenuViewComponent {
+export class MenuViewComponent implements OnInit {
+
+  bandas: any[] = [
+    {nome: 'banda teste1'},
+    {nome: 'banda teste2'}
+  ];
+
+  albuns: any[] = [
+    {nome: 'album teste1'},
+    {nome: 'album teste2'}
+  ];
+
+  constructor (private bandaService: BandaServiceService, private albumService: AlbumServiceService) {}
+
+    ngOnInit(): void {
+        this.carregarDados();
+    }
+    carregarDados(){
+    this.bandaService.getBandas().subscribe({
+      next: (dados) => this.bandas = dados,
+      error: (err) => console.error('Erro ao buscar bandas:', err)
+    });
+
+    this.albumService.getAlbuns().subscribe({
+      next: (dados) => this.albuns = dados,
+      error: (err) => console.error('Erro ao buscar álbuns:', err)
+    });
+  }
 
 }
 
